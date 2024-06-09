7e3ec2c353e247326f3faf5ab38864a23e8a8918
BekaDev05 <bekturspain@gmai.com>
2024-06-09 21:43:48 +0200

    Initial Commit

---
+++ Portfolio/PortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioApp.swift
+//  Portfolio
+//
+//  Created by Бектур Каримов on 9/6/24.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
